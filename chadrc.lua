25ae167122c0706b5dc4d290cb3257f3b4182883
kuba <kubawojdan110@gmail.com>
2023-07-03 16:14:42 +0100

    copilot support

--- chadrc.lua
+++ chadrc.lua
@@ -1,9 +1,6 @@
 ---@type ChadrcConfig
 local M = {}
 
--- Options
--- vim.opt.relativenumber = true
-
 -- Path to overriding theme and highlights files
 local highlights = require "custom.highlights"
 
@@ -13,7 +10,7 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
-
+  lsp_semantic_tokens = true,
   nvdash = {
     load_on_startup = true,
   },
