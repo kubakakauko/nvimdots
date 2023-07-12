43fbb1b786ecc3586a5736ad57ea3fd14500da81
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2023-07-12 17:17:12 +0100

    Autocmd

--- init.lua
+++ init.lua
@@ -12,3 +12,14 @@ autocmd("BufRead,BufNewFile", {
   pattern = "*",
   command = "set number relativenumber",
 })
+
+autocmd('User', {
+  pattern = 'GitConflictDetected',
+  callback = function()
+    vim.notify('Conflict detected in '..vim.fn.expand('<afile>'))
+    vim.keymap.set('n', 'cww', function()
+      engage.conflict_buster()
+      create_buffer_local_mappings()
+    end)
+  end
+})
