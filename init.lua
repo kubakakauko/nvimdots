364e1036cb60c1cdc4f535fad11c5c90bac9b46c
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2023-08-09 20:51:55 +0100

    Omni Presence

--- init.lua
+++ init.lua
@@ -3,8 +3,9 @@ local autocmd = vim.api.nvim_create_autocmd
 vim.g.OmniSharp_server_use_mono = 1
 vim.g.OmniSharp_server_use_net6 = 0
 vim.g.OmniSharp_loglevel = "DEBUG"
-vim.g.loaded_python3_provider=1
-vim.g.loaded_node_provider=1
+vim.g.loaded_python3_provider = 1
+vim.g.loaded_node_provider = 1
+
 
 -- Auto resize panes when resizing nvim window
 autocmd("VimResized", {
@@ -18,22 +19,22 @@ autocmd("BufRead,BufNewFile", {
   command = "set number relativenumber",
 })
 
-autocmd('User', {
-  pattern = 'GitConflictDetected',
+autocmd("User", {
+  pattern = "GitConflictDetected",
   callback = function()
-    vim.notify('Conflict detected in '..vim.fn.expand('<afile>'))
-    vim.keymap.set('n', 'cww', function()
+    vim.notify("Conflict detected in " .. vim.fn.expand "<afile>")
+    vim.keymap.set("n", "cww", function()
       engage.conflict_buster()
       create_buffer_local_mappings()
     end)
-  end
+  end,
 })
 
 -- Autocmd to change working directory to the current file's location
 autocmd("BufEnter", {
   pattern = "*",
   callback = function()
-    local path = vim.fn.expand('%:p:h')
-    vim.cmd('lcd ' .. path)
+    local path = vim.fn.expand "%:p:h"
+    vim.cmd("lcd " .. path)
   end,
 })
--- plugins.lua
+++ plugins.lua
@@ -386,6 +386,30 @@ local plugins = {
   -- Language-Specific Plugins (Rust Development)
   { "racer-rust/vim-racer", ft = "rust" },
   { "timonv/vim-cargo", ft = "rust" },
+  {
+    "TakenMC/presence.nvim",
+    lazy = false,
+    config = function()
+      require("presence").setup {
+        auto_update = true,
+        neovim_image_text = "Im still looking for the exit button for VIM?",
+        main_image = "neovim",
+        log_level = nil,
+        debounce_timeout = 100,
+        enable_line_number = false,
+        buttons = true,
+        show_time = true,
+        -- Rich Presence text options
+        editing_text = "Editing %s",
+        file_explorer_text = "Browsing %s",
+        git_commit_text = "Committing changes",
+        plugin_manager_text = "Managing plugins",
+        reading_text = "Reading %s",
+        workspace_text = "Working on %s",
+        line_number_text = "Line %s out of %s",
+      }
+    end,
+  },
 }
 
 return plugins
